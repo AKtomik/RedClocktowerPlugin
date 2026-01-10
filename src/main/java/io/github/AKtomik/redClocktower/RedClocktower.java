dd137e6850ea190af8c3bf89f8d31e71bd40982e
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-01-10 23:41:15 +0100

    some cleanup

--- src/main/java/io/github/AKtomik/redClocktower/RedClocktower.java
+++ src/main/java/io/github/AKtomik/redClocktower/RedClocktower.java
@@ -1,16 +1,8 @@
 package io.github.AKtomik.redClocktower;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
-import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.plugin.configuration.PluginMeta;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.command.TabCompleter;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
@@ -22,13 +14,11 @@ public final class RedClocktower extends JavaPlugin {
         // Plugin startup logic
         getLogger().info("Enabled!");
 
-//        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
-//            commands.registrar().register(new TagmeCommand().root.build());
-//        });
+        // list bridge commands
+        List<CommandBrigadierBase> bridgeCommandsSource = List
+        .of(new TagmeCommand(), new WhosendCommand(), new StorytellerCommand());
 
         // load bridge commands
-        List<CommandBrigadierBase> bridgeCommandsSource = List.of(new TagmeCommand(), new WhosendCommand(), new StorytellerCommand());
-
         this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
             for (CommandBrigadierBase brigadier : bridgeCommandsSource)
             {
@@ -36,20 +26,6 @@ public final class RedClocktower extends JavaPlugin {
                 commands.registrar().register(build, brigadier.aliases());
             }
         });
-    }
-
-    public PluginCommand loadCommand(String cmdStr, CommandExecutor cmdExe)
-    {
-        PluginCommand pluginCommand = getCommand(cmdStr);
-        if (pluginCommand == null) throw new IllegalStateException("§ccommand '%s' not registered in yml".formatted(cmdStr));
-        pluginCommand.setExecutor(cmdExe);
-        return  pluginCommand;
-    }
-    public PluginCommand loadCommand(String cmdStr, CommandExecutor cmdExe, TabCompleter cmdComplete)
-    {
-        PluginCommand pluginCommand = loadCommand(cmdStr, cmdExe);
-        pluginCommand.setTabCompleter(cmdComplete);
-        return pluginCommand;
     }
 
     @Override
--- src/main/java/io/github/AKtomik/redClocktower/StorytellerCommand.java
+++ src/main/java/io/github/AKtomik/redClocktower/StorytellerCommand.java
@@ -1,34 +1,9 @@
 package io.github.AKtomik.redClocktower;
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.arguments.ArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
-import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.command.brigadier.MessageComponentSerializer;
-import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
-import org.jspecify.annotations.NonNull;
-import org.jspecify.annotations.NullMarked;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 public class StorytellerCommand extends CommandBrigadierBase {
 
