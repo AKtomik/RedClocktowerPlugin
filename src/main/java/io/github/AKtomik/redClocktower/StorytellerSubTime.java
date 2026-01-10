d83dbc319b8688a7eddb75b931b5c3fedae97ef0
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-01-10 23:39:15 +0100

    sub time is in its own file and class

---
+++ src/main/java/io/github/AKtomik/redClocktower/StorytellerSubTime.java
@@ -0,0 +1,147 @@
+package io.github.AKtomik.redClocktower;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.MessageComponentSerializer;
+import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.jspecify.annotations.NullMarked;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public class StorytellerSubTime extends SubBrigadierBase {
+
+	public String name() {
+		return "time";
+	}
+
+	public LiteralArgumentBuilder<CommandSourceStack> root() {
+		return Commands.literal("time")
+		.then(Commands.argument("period", new BloodDayPeriodArgument())
+		.executes(ctx -> {
+			World world;
+			CommandSender sender = ctx.getSource().getSender();
+			Entity executor = ctx.getSource().getExecutor();
+
+			Location location = ctx.getSource().getLocation();
+			world = location.getWorld();
+			final BloodDayPeriod dayPeriod = ctx.getArgument("period", BloodDayPeriod.class);
+
+			sender.sendMessage(
+			Component.text("switching to time ").color(NamedTextColor.LIGHT_PURPLE)
+			.append(Component.text(dayPeriod.toString()).color(NamedTextColor.LIGHT_PURPLE).decorate(TextDecoration.BOLD))
+			.append(Component.text("").color(NamedTextColor.LIGHT_PURPLE))
+			);
+			dayPeriodsStartAction.get(dayPeriod).accept(world);
+			return Command.SINGLE_SUCCESS;
+		}));
+	}
+
+	Map<BloodDayPeriod, Consumer<World>> dayPeriodsStartAction = Map.ofEntries(
+	Map.entry(BloodDayPeriod.MORNING, (world) -> {
+		Bukkit.getServer().broadcast(
+		Component.text("it's the morning!").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD)
+		);
+		Bukkit.getServer().broadcast(
+		Component.text("everyone to the townhall").color(NamedTextColor.GRAY).decorate(TextDecoration.ITALIC)
+		);
+		world.setTime(0);
+	}),
+	Map.entry(BloodDayPeriod.FREE, (world) -> {
+		Bukkit.getServer().broadcast(
+		Component.text("wonder time").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD)
+		);
+		Bukkit.getServer().broadcast(
+		Component.text("you are free to go").color(NamedTextColor.GRAY).decorate(TextDecoration.ITALIC)
+		);
+		world.setTime(6000);
+	}),
+	Map.entry(BloodDayPeriod.MEET, (world) -> {
+		Bukkit.getServer().broadcast(
+		Component.text("debate time").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD)
+		);
+		Bukkit.getServer().broadcast(
+		Component.text("everyone to the townhall").color(NamedTextColor.GRAY).decorate(TextDecoration.ITALIC)
+		);
+		world.setTime(12000);
+	}),
+	Map.entry(BloodDayPeriod.NIGHT, (world) -> {
+		Bukkit.getServer().broadcast(
+		Component.text("the moon is rising...").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD)
+		);
+		Bukkit.getServer().broadcast(
+		Component.text("go to your house and sleep well").color(NamedTextColor.GRAY).decorate(TextDecoration.ITALIC)
+		);
+		world.setTime(18000);
+	})
+	);
+}
+
+
+@NullMarked
+enum BloodDayPeriod {
+	MORNING,
+	FREE,
+	MEET,
+	NIGHT;
+
+	@Override
+	public String toString() {
+		return name().toLowerCase();
+	}
+}
+
+@NullMarked
+class BloodDayPeriodArgument implements CustomArgumentType.Converted<BloodDayPeriod, String> {
+
+	private static final DynamicCommandExceptionType ERROR_INVALID_PERIOD = new DynamicCommandExceptionType(flavor -> {
+		return MessageComponentSerializer.message().serialize(Component.text(flavor + " is not a blood day period!"));
+	});
+
+	@Override
+	public BloodDayPeriod convert(String nativeType) throws CommandSyntaxException {
+		try {
+			return BloodDayPeriod.valueOf(nativeType.toUpperCase(Locale.ROOT));
+		} catch (IllegalArgumentException ignored) {
+			throw ERROR_INVALID_PERIOD.create(nativeType);
+		}
+	}
+
+	@Override
+	public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+		for (BloodDayPeriod flavor : BloodDayPeriod.values()) {
+			String name = flavor.toString();
+
+			// Only suggest if the flavor name matches the user input
+			if (name.startsWith(builder.getRemainingLowerCase())) {
+				builder.suggest(flavor.toString());
+			}
+		}
+
+		return builder.buildFuture();
+	}
+
+	@Override
+	public ArgumentType<String> getNativeType() {
+		return StringArgumentType.word();
+	}
+}
