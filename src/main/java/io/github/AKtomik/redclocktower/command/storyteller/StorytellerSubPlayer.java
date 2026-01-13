18705be3efe8c44831f6d2619c9c599ddac0ffd0
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-01-13 20:45:54 +0100

    rework sub player code structure

--- src/main/java/io/github/AKtomik/redclocktower/command/storyteller/StorytellerSubPlayer.java
+++ src/main/java/io/github/AKtomik/redclocktower/command/storyteller/StorytellerSubPlayer.java
@@ -2,6 +2,7 @@ package io.github.AKtomik.redclocktower.command.storyteller;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import io.github.AKtomik.redclocktower.brigadier.SubBrigadierBase;
 import io.github.AKtomik.redclocktower.game.BloodGame;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
@@ -13,6 +14,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class StorytellerSubPlayer extends SubBrigadierBase {
 
@@ -22,101 +24,82 @@ public class StorytellerSubPlayer extends SubBrigadierBase {
 		return base()
 		.then(Commands.literal("add")
 			.then(Commands.argument("players", ArgumentTypes.players())
-			.executes(
-			ctx -> {
-				final PlayerSelectorArgumentResolver targetResolver = ctx.getArgument("players", PlayerSelectorArgumentResolver.class);
-				final List<Player> targets = targetResolver.resolve(ctx.getSource());
-				final CommandSender sender = ctx.getSource().getSender();
-				final BloodGame game = BloodGame.get(ctx.getSource().getLocation().getWorld());
-
-				// checks
-				if (!game.isReady())
-				{
-					sender.sendRichMessage("<red>the game is not ready!");
-					return Command.SINGLE_SUCCESS;
-				}
-				if (targets.isEmpty())
-				{
-					sender.sendRichMessage("<red>no player found");
-					return Command.SINGLE_SUCCESS;
-				}
-
-				// the action
-				for (Player player : targets)
-				{
-					if (game.isPlayerIn(player))
-					{
-						sender.sendRichMessage("<red><b><target></b> is already in game.",
-						Placeholder.parsed("target", player.getName())
-						);
-					} else {
-						game.addPlayer(player);
-						sender.sendRichMessage("you added <b><target></b>.",
-						Placeholder.parsed("target", player.getName())
-						);
-					}
-				}
-
-//				if (targets.size() == 1)
-//					sender.sendRichMessage("you added <b><target></b>.",
-//					Placeholder.parsed("target", targets.getFirst().getName())
-//					);
-//				else
-//					sender.sendRichMessage("you added <b><amount></b> players.",
-//					Placeholder.parsed("amount", Integer.toString(targets.size()))
-//					);
-				return Command.SINGLE_SUCCESS;
-			}
-			)))
+			.executes(subCommandExeAdd)))
 
 		.then(Commands.literal("remove")
 			.then(Commands.argument("players", ArgumentTypes.players())
-			.executes(
-			ctx -> {
-				final PlayerSelectorArgumentResolver targetResolver = ctx.getArgument("players", PlayerSelectorArgumentResolver.class);
-				final List<Player> targets = targetResolver.resolve(ctx.getSource());
-				final CommandSender sender = ctx.getSource().getSender();
-				final BloodGame game = BloodGame.get(ctx.getSource().getLocation().getWorld());
-
-				// checks
-				if (!game.isReady())
-				{
-					sender.sendRichMessage("<red>the game is not ready!");
-					return Command.SINGLE_SUCCESS;
-				}
-				if (targets.isEmpty())
-				{
-					sender.sendRichMessage("<red>no player found");
-					return Command.SINGLE_SUCCESS;
-				}
-
-				// the action
-				for (Player player : targets)
-				{
-					if (!game.isPlayerIn(player))
-					{
-						sender.sendRichMessage("<red><b><target></b> is not in game.",
-						Placeholder.parsed("target", player.getName())
-						);
-					} else {
-						game.removePlayer(player);
-						sender.sendRichMessage("you removed <b><target></b>.",
-						Placeholder.parsed("target", player.getName())
-						);
-					}
-				}
-
-//				if (targets.size() == 1)
-//					sender.sendRichMessage("you removed <b><target></b>.",
-//					Placeholder.parsed("target", targets.getFirst().getName())
-//					);
-//				else
-//					sender.sendRichMessage("you removed <b><amount></b> players.",
-//					Placeholder.parsed("amount", Integer.toString(targets.size()))
-//					);
-				return Command.SINGLE_SUCCESS;
-			}
-			))
+			.executes(subCommandExeRemove))
 		);
 	}
+
+	Command<CommandSourceStack> subCommandExeAdd = ctx -> {
+		final List<Player> players = ctx.getArgument("players", PlayerSelectorArgumentResolver.class).resolve(ctx.getSource());
+		final CommandSender sender = ctx.getSource().getSender();
+		final BloodGame game = BloodGame.get(ctx.getSource().getLocation().getWorld());
+
+		// checks
+		if (!game.isReady())
+		{
+			sender.sendRichMessage("<red>the game is not ready!");
+			return Command.SINGLE_SUCCESS;
+		}
+		if (players.isEmpty())
+		{
+			sender.sendRichMessage("<red>no player found");
+			return Command.SINGLE_SUCCESS;
+		}
+
+		// the action
+		for (Player player : players)
+		{
+			if (game.isPlayerIn(player))
+			{
+				sender.sendRichMessage("<red><b><target></b> is already in game.",
+				Placeholder.parsed("target", player.getName())
+				);
+			} else {
+				game.addPlayer(player);
+				sender.sendRichMessage("you added <b><target></b>.",
+				Placeholder.parsed("target", player.getName())
+				);
+			}
+		}
+		return Command.SINGLE_SUCCESS;
+	};
+
+	public Command<CommandSourceStack> subCommandExeRemove = ctx -> {
+		final PlayerSelectorArgumentResolver targetResolver = ctx.getArgument("players", PlayerSelectorArgumentResolver.class);
+		final List<Player> players = targetResolver.resolve(ctx.getSource());
+		final CommandSender sender = ctx.getSource().getSender();
+		final BloodGame game = BloodGame.get(ctx.getSource().getLocation().getWorld());
+
+		// checks
+		if (!game.isReady())
+		{
+			sender.sendRichMessage("<red>the game is not ready!");
+			return Command.SINGLE_SUCCESS;
+		}
+		if (players.isEmpty())
+		{
+			sender.sendRichMessage("<red>no player found");
+			return Command.SINGLE_SUCCESS;
+		}
+
+		// the action
+		for (Player player : players)
+		{
+			if (!game.isPlayerIn(player))
+			{
+				sender.sendRichMessage("<red><b><target></b> is not in game.",
+				Placeholder.parsed("target", player.getName())
+				);
+			} else {
+				game.removePlayer(player);
+				sender.sendRichMessage("you removed <b><target></b>.",
+				Placeholder.parsed("target", player.getName())
+				);
+			}
+		}
+		return Command.SINGLE_SUCCESS;
+	};
 }
