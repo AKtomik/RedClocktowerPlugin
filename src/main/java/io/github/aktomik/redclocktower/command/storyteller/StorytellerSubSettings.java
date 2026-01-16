89fcd9276a894d40149c4206c9aa98b0e4b14b5e
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-01-16 01:49:35 +0100

    change slots amount command

--- src/main/java/io/github/aktomik/redclocktower/command/storyteller/StorytellerSubSettings.java
+++ src/main/java/io/github/aktomik/redclocktower/command/storyteller/StorytellerSubSettings.java
@@ -1,11 +1,15 @@
 package io.github.aktomik.redclocktower.command.storyteller;
 
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.github.aktomik.redclocktower.game.BloodGame;
+import io.github.aktomik.redclocktower.game.BloodGamePeriod;
 import io.github.aktomik.redclocktower.utils.brigadier.SubBrigadierBase;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.command.CommandSender;
 
 public class StorytellerSubSettings extends SubBrigadierBase {
 
@@ -13,17 +17,35 @@ public class StorytellerSubSettings extends SubBrigadierBase {
 
 	public LiteralArgumentBuilder<CommandSourceStack> root() {
 		return base()
-		.executes(subSlotLimitCheck)
-		.then(Commands.argument("new value", ArgumentTypes.integerRange())
-			.executes(subSlotLimitChange)
+		.then(Commands.literal("solt_limit"))
+			.executes(subSlotLimitCheck)
+			.then(Commands.argument("new value", IntegerArgumentType.integer(0, 20))
+				.executes(subSlotLimitChange)
 		);
 	}
 
 	Command<CommandSourceStack> subSlotLimitCheck = ctx -> {
+		// arguments
+		CommandSender sender = ctx.getSource().getSender();
+		BloodGame game = BloodGame.get(ctx.getSource().getLocation().getWorld());
+
+		final int value = game.getSettingsSlotLimit();
+		sender.sendRichMessage("set slot limit to <b><aqua><value></aqua></b>.",
+			Placeholder.parsed("value", Integer.toString(value))
+		);
 		return Command.SINGLE_SUCCESS;
 	};
 
 	Command<CommandSourceStack> subSlotLimitChange = ctx -> {
+		// arguments
+		CommandSender sender = ctx.getSource().getSender();
+		BloodGame game = BloodGame.get(ctx.getSource().getLocation().getWorld());
+		final int newValue = ctx.getArgument("new value", int.class);
+
+		game.changeSlotLimit(newValue);
+		sender.sendRichMessage("set slot limit to <b><aqua><value></aqua></b>.",
+			Placeholder.parsed("value", Integer.toString(newValue))
+		);
 		return Command.SINGLE_SUCCESS;
 	};
 }
