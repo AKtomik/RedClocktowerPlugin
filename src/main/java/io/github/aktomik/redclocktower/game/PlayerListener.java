81e3fd63c3e022a793a3747a4242d186d7477c81
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-01-17 02:44:30 +0100

    lever click should vote

--- src/main/java/io/github/aktomik/redclocktower/game/PlayerListener.java
+++ src/main/java/io/github/aktomik/redclocktower/game/PlayerListener.java
@@ -1,10 +1,18 @@
 package io.github.aktomik.redclocktower.game;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Powerable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Objects;
 
 public class PlayerListener implements Listener {
 
@@ -21,5 +29,33 @@ public class PlayerListener implements Listener {
 		BloodPlayer bloodPlayer = BloodPlayer.get(player);
 		bloodPlayer.leaveGame();
 	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onLeverClick(PlayerInteractEvent event) {
+		if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+		Block block = event.getClickedBlock();
+		if (block == null || block.getType() != Material.LEVER) return;
+
+		Player player = event.getPlayer();
+		BloodPlayer bloodPlayer = BloodPlayer.get(player);
+		BloodGame game = bloodPlayer.getGame();
+		if (game == null) return;
+		BloodSlot slot = game.getSlot(bloodPlayer.getSlotIndex());
+
+		if (!Objects.equals(block.getLocation(),slot.getPosition(BloodSlotPlace.LEVER))) return;
+
+		// stop redstone
+		event.setCancelled(true);
+		// fake pull
+		BlockData data = block.getBlockData();
+		Powerable powerable = (Powerable)data;
+		powerable.setPowered(!powerable.isPowered());
+		block.setBlockData(powerable, false);
+
+		if (!bloodPlayer.canVote()) return;
+		player.sendRichMessage(powerable.isPowered() ? "own lever ON" : "own lever OFF");
+		bloodPlayer.changeVotePull(powerable.isPowered());
+	}
+
 }
 
