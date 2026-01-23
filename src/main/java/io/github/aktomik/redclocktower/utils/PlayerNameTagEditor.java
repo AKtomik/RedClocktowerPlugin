0192671c93c8f56116fdec2d8043a158f6ef5f3f
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-01-23 02:59:42 +0100

    tag editor upgrade

--- src/main/java/io/github/aktomik/redclocktower/utils/PlayerNameTagEditor.java
+++ src/main/java/io/github/aktomik/redclocktower/utils/PlayerNameTagEditor.java
@@ -1,14 +1,12 @@
 package io.github.aktomik.redclocktower.utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Display;
 import org.bukkit.entity.Player;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.TextDisplay;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 
 import java.util.HashMap;
@@ -18,6 +16,7 @@ import java.util.UUID;
 public class PlayerNameTagEditor {
 
 	private static final Map<UUID, TextDisplay> displays = new HashMap<>();
+	private static final Map<UUID, Location> forcePlace = new HashMap<>();
 
 	public static final float ABOVE_HEIGHT = 2f;
 
@@ -53,10 +52,26 @@ public class PlayerNameTagEditor {
 		}
 	}
 
+	public static void forcePlace(Player player, Location loc) {
+		forcePlace.put(player.getUniqueId(), loc);
+	}
+
+	public static void forceUnplace(Player player) {
+		forcePlace.remove(player.getUniqueId());
+	}
+
 	static void updatePosition(Player player) {
-		TextDisplay display = displays.get(player.getUniqueId());
+		UUID uuid = player.getUniqueId();
+		TextDisplay display = displays.get(uuid);
 		if (display != null && display.isValid()) {
-			Location newLoc = player.getLocation().add(0, ABOVE_HEIGHT, 0);
+			Location newLoc;
+			Location forceLoc = forcePlace.get(uuid);
+			if (player.getGameMode() == GameMode.SPECTATOR)
+				newLoc = new Location(player.getWorld(), 0,-100, 0);
+			else if (forceLoc != null)
+				newLoc = forceLoc;
+			else
+				 newLoc = player.getLocation().add(0, ABOVE_HEIGHT, 0);
 			display.teleport(newLoc);
 		}
 	}
