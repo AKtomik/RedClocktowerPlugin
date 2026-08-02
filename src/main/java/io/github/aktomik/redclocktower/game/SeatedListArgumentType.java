0b274116ac254f286e1d9b33f6d649a569daddfe
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-08-02 16:55:08 +0200

    StarWordArgumentType so SeatedListArgumentType can really take star

--- src/main/java/io/github/aktomik/redclocktower/game/SeatedListArgumentType.java
+++ src/main/java/io/github/aktomik/redclocktower/game/SeatedListArgumentType.java
@@ -41,7 +41,7 @@ public class SeatedListArgumentType implements CustomArgumentType<List<Seated>, 
 
 	@Override
 	public @NonNull ArgumentType<String> getNativeType() {
-		return StringArgumentType.string();
+		return StarWordArgumentType.starWord();
 	}
 
 	@Override
---
+++ src/main/java/io/github/aktomik/redclocktower/game/StarWordArgumentType.java
@@ -0,0 +1,32 @@
+package io.github.aktomik.redclocktower.game;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+
+import java.util.Collection;
+import java.util.List;
+
+public class StarWordArgumentType implements ArgumentType<String> {
+
+	public static StarWordArgumentType starWord() {
+		return new StarWordArgumentType();
+	}
+
+	@Override
+	public String parse(StringReader reader) {
+		int start = reader.getCursor();
+		while (reader.canRead() && isAllowed(reader.peek())) {
+			reader.skip();
+		}
+		return reader.getString().substring(start, reader.getCursor());
+	}
+
+	private static boolean isAllowed(char c) {
+		return StringReader.isAllowedInUnquotedString(c) || c == '*';
+	}
+
+	@Override
+	public Collection<String> getExamples() {
+		return List.of("word", "words_with_underscores", "*", "word_with*star");
+	}
+}
