653de846fa5c2705ca55547797f92069cecbc7a0
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-08-08 15:36:53 +0200

    tag handle death

--- src/main/java/io/github/aktomik/redclocktower/utils/PlayerNameTagEditorListener.java
+++ src/main/java/io/github/aktomik/redclocktower/utils/PlayerNameTagEditorListener.java
@@ -1,11 +1,15 @@
 package io.github.aktomik.redclocktower.utils;
 
+import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import static io.github.aktomik.redclocktower.utils.PlayerNameTagEditor.clearDisplay;
 import static io.github.aktomik.redclocktower.utils.PlayerNameTagEditor.syncDisplay;
@@ -25,5 +29,17 @@ public class PlayerNameTagEditorListener implements Listener {
 	@EventHandler
 	public void onGameModeChange(PlayerGameModeChangeEvent event) {
 		setVisibleDisplay(event.getPlayer(), event.getNewGameMode() != GameMode.SPECTATOR);
+	}
+
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event) {
+		setVisibleDisplay(event.getPlayer(), false);
+	}
+
+	@EventHandler
+	public void onRespawn(PlayerPostRespawnEvent event) {
+		Player player = event.getPlayer();
+		syncDisplay(player);
+		setVisibleDisplay(player, player.getGameMode() != GameMode.SPECTATOR);
 	}
 }
