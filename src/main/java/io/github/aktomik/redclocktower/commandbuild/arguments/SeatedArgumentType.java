79a8ce9b8a5671133c568bf2fe0667685d1b6c80
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-08-09 18:50:31 +0200

    fatal argument type error fixed

--- src/main/java/io/github/aktomik/redclocktower/commandbuild/arguments/SeatedArgumentType.java
+++ src/main/java/io/github/aktomik/redclocktower/commandbuild/arguments/SeatedArgumentType.java
@@ -2,6 +2,7 @@ package io.github.aktomik.redclocktower.commandbuild.arguments;
 
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
@@ -32,7 +33,7 @@ public class SeatedArgumentType implements CustomArgumentType<Seated, String> {
 
 	@Override
 	public @NonNull ArgumentType<String> getNativeType() {
-		throw new UnsupportedOperationException("requiring a CommandSourceStack source");
+		return StringArgumentType.word();
 	}
 
 	@Override
